6c1e6328048095fe9fe3185ad37b99bbf34eb69a
joelblr <95953794+joelblr@users.noreply.github.com>
2023-02-24 09:51:56 +0530

    Add files via upload

---
+++ calci/Fractions.java
@@ -0,0 +1,20 @@
+package calci;
+
+public class Fractions {
+
+	private int numer;
+	private int denom;
+
+	public Fractions() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getNumerator() {
+		return this.numer;
+
+	}public int getDenominator() {
+		return this.denom;
+	}
+
+
+}
