384b56233ca4790b3c9abdfde0dfc47bee180e8f
joelblr <95953794+joelblr@users.noreply.github.com>
2023-03-06 12:58:03 +0530

    Add files via upload

--- calci/CalciMain.java
+++ calci/CalciMain.java
@@ -2,96 +2,18 @@ package calci;
 
 import design.*;
 import errors.*;
-
 import java.util.*;
 
 import calci.mode_1.*;
 import calci.mode_2.*;
-//import calci.mode_4.*;
-//import calci.mode_5.*;
-
+//import calci.mode_3.*;
+import calci.mode_4.*;
+import calci.mode_5.*;
+import calci.mode_6.*;
 
 
 public class CalciMain {
 
-	private static String angle;
-
-	static {
-		angle = "rad";	// deg or rad
-	}
-
-	public static void setAngleMode(String mode) {
-		angle = mode;
-
-	}public static String getAngleMode() {
-		return angle;
-	}
-
-//	private static String showDisplay(String tag) {
-//		List<String> stdin = 
-//			Design.printBox(50,
-//				new DLabel("", "<top>", ""),
-//				new DLabel(tag, "<tag>", "Y"),
-//				new DLabel("", "<input>", "C"),
-//				new DLabel("", "<base>", "")
-//		);
-//		return stdin.get(0);
-//	}
-
-	public static void showSetup() {
-
-		while (true) {
-			Design.clearScreen();
-			if (Design.errorFlag)
-				Design.showErrorMessage(Design.errorMsg);
-	
-			List<String> stdin = 
-				Design.printBox(0,
-					new DLabel("", "<top>", ""),
-					new DLabel("Casio fx-991+ Setup", "<tag>", "Y"),
-					new DLabel("", "<blank>", ""),
-					new DLabel(" *1*  Degree", "", "C"),
-					new DLabel(" *2*  Radian", "", "C"),
-					new DLabel(" *0*  Close Setup", "", "C"),
-					new DLabel("", "<joint>", ""),
-					new DLabel("Enter Choice:  ", "<input>", "P"),
-					new DLabel("", "<base>", "")
-				);
-
-			try {
-	
-				Design.IntegerChoice = Integer.parseInt(stdin.get(0).trim());
-				Design.loadingProcess(1500);
-	
-				switch (Design.IntegerChoice) {
-	
-					case 1 :
-						setAngleMode("deg");
-						return;
-	
-					case 2 :
-						setAngleMode("rad");
-						return;
-	
-					case 0 :
-						return;
-	
-					default :
-						throw new NumberFormatException("<INVALID CHOICE!>\nChoices are: 0/1/2");
-				}
-	
-			}catch (NumberFormatException e) {
-				Design.loadingProcess(600);
-				if (!e.getMessage().startsWith("<"))
-					Design.errorMsg = "<INVALID INPUT!>\nChoice must be 0/1/2";
-				else
-					Design.errorMsg = e.getMessage();
-	
-				Design.errorFlag = true;
-	
-			}
-		}
-	}
 
 	public static void showMenu() {
 
@@ -101,13 +23,13 @@ public class CalciMain {
 				new DLabel("Casio fx-991+ Modes", "<tag>", "Y"),
 				new DLabel("", "<joint>", ""),
 				new DLabel(" *1*  Scientific", "", "C"),
-				new DLabel(" *2*  Programming", "", "C"),
-				new DLabel(" *3*  Complex Mode", "", "R"),
-				new DLabel(" *4*  Base-N Mode", "", "C"),
+				new DLabel(" *2*  Complex Mode", "", "C"),
+//				new DLabel(" *3*  Statistics", "", "R"),
+//				new DLabel(" *4*  Base-N Mode", "", "C"),
 				new DLabel(" *5*  Equation Mode", "", "C"),
-				new DLabel(" *6*  Matrix Mode", "", "R"),
-				new DLabel(" *7*  Table Mode", "", "R"),
-				new DLabel(" *8*  Vector Mode", "", "R"),
+				new DLabel(" *6*  Programming", "", "R"),
+//				new DLabel(" *7*  Table Mode", "", "R"),
+//				new DLabel(" *8*  Vector Mode", "", "R"),
 				new DLabel(" *9*  Setup Mode", "", "C"),
 				new DLabel(" *0*  Close Casio fx-707", "", "C"),
 				new DLabel("", "<joint>", ""),
@@ -119,6 +41,16 @@ public class CalciMain {
 
 	}
 
+	public static String showDisplay(String tag) {
+		List<String> stdin = 
+			Design.printBox(50,
+				new DLabel("", "<top>", ""),
+				new DLabel(tag, "<tag>", "Y"),
+				new DLabel("", "<input>", "C"),
+				new DLabel("", "<base>", "")
+		);
+		return stdin.get(0);
+	}
 
 	public static void run() {
 		CalciRun();
@@ -132,10 +64,9 @@ public class CalciMain {
 			try {
 
 				Design.clearScreen();
-				if (Design.errorFlag) {
+				if (Design.errorFlag)
 					Design.showErrorMessage(Design.errorMsg);
-					Design.errorFlag = false;
-				}
+
 				showMenu();
 				Design.loadingProcess(1500);
 
@@ -146,23 +77,27 @@ public class CalciMain {
 						break;
 	
 					case 2 :
+						KomplxMain.run();
+						break;
+
+//					case 3 ://TODO XXX
+////						StatMain.run();
+//						break;
+//
+//					case 4 ://TODO XXX
+//						NBaseMain.run();
+//						break;
+
+					case 5 :
+						EqnMain.run();
+						break;
+
+					case 6 :
 						BoolMain.run();
 						break;
 
-					case 3 :
-//						Expressions.booleanRun();
-						break;
-
-					case 4 :
-//						NBaseMain.run();
-						break;
-
-//					case 5 :
-//						EqnMain.run();
-//						break;
-
 					case 9 :
-						showSetup();
+						Setup.showSetup();
 						break;
 
 					case 0 :
@@ -188,7 +123,7 @@ public class CalciMain {
 
 				Design.errorFlag = true;
 
-			}catch (ManyInputsException e) {
+			}catch (InputsFreqException e) {
 				Design.loadingProcess(600);
 				Design.errorFlag = true;
 
